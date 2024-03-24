6ebef4a55452b13ffe9a7eca19ef262aab853124
Sean_Chung <sean2616607@gmail.com>
2024-03-25 00:34:12 +0800

    影片調整

---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject video;
+    public GameObject UI;   // 一開始城市場景黑畫面 當影片一播放就關閉
+    void Start() 
+    {
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.started += OnVideoStarted;
+    }
+
+    private void OnVideoStarted(VideoPlayer source)
+    {
+        Debug.Log("Video start");
+        UI.SetActive(false);
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        Debug.Log("Video Finished");
+        video.gameObject.SetActive(false);
+    }
+
+}
